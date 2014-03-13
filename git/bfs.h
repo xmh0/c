f06608605806aecc730e335de889889efc64bb2e
xmh <xmh@192.168.102.1>
2014-03-14 02:39:31 +0800

    xmh

---
+++ git/bfs.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   bfs.h
+ * Author: xmh
+ *
+ * Created on 2014年3月14日, 上午1:29
+ */
+
+#ifndef BFS_H
+#define	BFS_H
+
+EDGE *bfs(EDGE *matrix, int vertex_no, int vertex_count);
+void print_path(EDGE **parent, int s);
+void print_path(EDGE **parent, int s);
+
+#endif	/* BFS_H */
