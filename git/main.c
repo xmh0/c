b09e1466bfe9efe4b18dd63625dee595717fb8bf
xmh <xmh@192.168.102.1>
2014-03-14 00:28:02 +0800

    xmh

---
+++ git/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: xmh
+ *
+ * Created on 2014年3月14日, 上午12:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+        printf("hello world");
+        return (EXIT_SUCCESS);
+}
+
