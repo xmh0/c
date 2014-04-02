b620ac8fe4139101a8fe2b0439ee11b28c7615f9
xmh <xmh@192.168.1.127>
2014-04-02 18:18:58 +0800

    xmh

---
+++ git/min_heap.c
@@ -0,0 +1,18 @@
+
+#include"heap.h"
+
+HEAP *create_min_heap(int size){
+        return create_heap(size);
+}
+void insert_min_heap(HEAP *heap, HEAP_NODE *heap_node){
+        insert_heap(heap, heap_node, MIN);
+}
+void sort_min_heap(HEAP *heap){
+        sort_heap(heap, MIN);
+}
+HEAP_NODE *minimum(HEAP *heap){
+        return extrem_heap(heap);
+}
+HEAP_NODE *extract_min_heap(HEAP *heap){
+        return extract_heap(heap, MIN);
+}
---
+++ git/min_heap.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   min_heap.h
+ * Author: xmh
+ *
+ * Created on 2014年4月2日, 下午6:17
+ */
+
+#ifndef MIN_HEAP_H
+#define	MIN_HEAP_H
+
+HEAP *create_min_heap(int size);
+void insert_min_heap(HEAP *heap, HEAP_NODE *heap_node);
+void sort_min_heap(HEAP *heap);
+HEAP_NODE *minimum(HEAP *heap);
+HEAP_NODE *extract_min_heap(HEAP *heap);
+
+#endif	/* MIN_HEAP_H */
+
